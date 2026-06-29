660560c37e55bde8c9901741a9b2c86742856f9c
Abdulhafiz Yusuf <talk2abdulhafiz@gmail.com>
2026-06-29 06:27:33 +0100

    Initialize Savings Goal API project with Flyway and MySQL configuration

---
+++ src/main/java/com/ay_smart_tech/savings_goal_api/SavingsGoalApiApplication.java
@@ -0,0 +1,13 @@
+package com.ay_smart_tech.savings_goal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsGoalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavingsGoalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ay_smart_tech/savings_goal_api/SavingsGoalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ay_smart_tech.savings_goal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsGoalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
